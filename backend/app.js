97729c4bcec4843cbfd9c61f16319d4e42c8c8be
Kumar Harsh <kharsh39@gmail.com>
2024-01-26 16:28:41 +0530

    Update MFA enroll feature

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,18 @@ const stytch = require("stytch")
 const cookieParser = require("cookie-parser")
 require('dotenv').config()
 const cors = require('cors')
-const { getIntermediateSession, setIntermediateSession, setSession, clearSession, clearIntermediateSession, revokeSession, getDiscoverySessionData } = require("./session")
+const {
+    getIntermediateSession,
+    setIntermediateSession,
+    setSession,
+    clearSession,
+    clearIntermediateSession,
+    revokeSession,
+    getDiscoverySessionData
+} = require("./session")
 
 const stytchEnv =
-    process.env.NEXT_PUBLIC_STYTCH_PROJECT_ENV === "live"
+    process.env.STYTCH_PROJECT_ENV === "live"
         ? stytch.envs.live
         : stytch.envs.test;
 
@@ -52,18 +60,18 @@ app.post("/api/discovery/create", async (req, res) => {
     const intermediateSession = getIntermediateSession(req, res);
 
     if (!intermediateSession) {
-        return res.redirect(307, "/login");
+        res.status(400).json({message: "Not logged in", redirectPath: "/login"})
     }
-    const { organization_name, require_mfa } = req.body;
+    const { organizationName, requireMfa } = req.body;
 
     try {
         const { member, organization, session_jwt, intermediate_session_token } =
             await stytchClient.discovery.organizations.create({
                 intermediate_session_token: intermediateSession,
                 email_allowed_domains: [],
-                organization_name: organization_name,
+                organization_name: organizationName,
                 session_duration_minutes: 60,
-                mfa_policy: require_mfa ? "REQUIRED_FOR_ALL" : "OPTIONAL"
+                mfa_policy: requireMfa ? "REQUIRED_FOR_ALL" : "OPTIONAL"
             });
 
         // Make the organization discoverable to other emails
@@ -89,16 +97,19 @@ app.post("/api/discovery/create", async (req, res) => {
             trusted_metadata: { admin: true },
         });
 
+        // Check if the organization requires MFA before logging in
         if (session_jwt === "") {
             setIntermediateSession(req, res, intermediate_session_token)
             clearSession(req, res)
             res.status(200).json({ message: "Organization created. Proceed for MFA", redirectPath: `/smsmfa?sent=false&org_id=${organization.organization_id}&member_id=${member.member_id}`, organizationId: organization.organization_id });
             return
         }
+
+        // If not, set the session values and redirect the user to the dashboard
         clearIntermediateSession(req, res);
         setSession(req, res, session_jwt);
         res.status(200).json({ message: "Organization created", redirectPath: `/dashboard`, organizationId: organization.organization_id });
-        return
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Something went wrong", redirectPath: `/login` });
@@ -108,8 +119,10 @@ app.post("/api/discovery/create", async (req, res) => {
 app.post("/api/discovery/get-organization", async (req, res) => {
     const { organizationId, memberEmail } = req.body
 
+    // If no organization was sent, revoke the session and make the user log in again
     if (!organizationId || Array.isArray(organizationId)) {
-        res.status(500).json({ message: "No org ID found", redirectPath: "/login" });
+        revokeSession(req, res, stytchClient)
+        res.status(500).json({ message: "No organization ID found", redirectPath: "/login" });
         return
     }
 
@@ -124,6 +137,7 @@ app.post("/api/discovery/get-organization", async (req, res) => {
 
 app.post("/api/discovery/select-organization", async (req, res) => {
 
+    // If no session found, redirect users to log in
     const discoverySessionData = getDiscoverySessionData(req, res);
     if (discoverySessionData.error) {
         console.error("No session tokens found...");
@@ -134,32 +148,26 @@ app.post("/api/discovery/select-organization", async (req, res) => {
     const { organizationId } = req.body
 
     if (!organizationId || Array.isArray(organizationId)) {
-        res.status(500).json({ message: "No org ID found", redirectPath: "/discovery" });
+        res.status(500).json({ message: "No organization ID found", redirectPath: "/discovery" });
         return
     }
 
-    const exchangeSession = () => {
-        if (discoverySessionData.isDiscovery) {
-            return stytchClient.discovery.intermediateSessions.exchange({
-                intermediate_session_token: discoverySessionData.intermediateSession,
-                organization_id: organizationId,
-                session_duration_minutes: 60,
-            });
-        }
-        return stytchClient.sessions.exchange({
-            organization_id: organizationId,
-            session_jwt: discoverySessionData.sessionJWT,
-        });
-    };
-
     try {
-        const { session_jwt, organization, member, intermediate_session_token } = await exchangeSession();
+        const { session_jwt, organization, member, intermediate_session_token } = await stytchClient.discovery.intermediateSessions.exchange({
+            intermediate_session_token: discoverySessionData.intermediateSession,
+            organization_id: organizationId,
+            session_duration_minutes: 60,
+        });
+        
+        // Check if the organization requires MFA before logging in
         if (session_jwt === "") {
             setIntermediateSession(req, res, intermediate_session_token)
             clearSession(req, res)
             res.status(200).json({ message: "2FA started", redirectPath: `/smsmfa?sent=false&org_id=${organization.organization_id}&member_id=${member.member_id}`, organizationId: organization.organization_id })
             return
         }
+
+        // If not, set the session values and redirect the user to the dashboard
         setSession(req, res, session_jwt);
         clearIntermediateSession(req, res);
         res.status(200).json({ message: "Auth complete", redirectPath: `/dashboard`, organizationId: organization.organization_id });
@@ -196,19 +204,19 @@ app.get("/api/auth/logout", async (req, res) => {
 })
 
 app.post("/api/mfa/send", async (req, res) => {
-    const { orgID, memberId, phone_number } = req.body;
+    const { organizationId, memberId, phoneNumber } = req.body;
 
     try {
         const resp = await stytchClient.otps.sms.send({
-            organization_id: orgID,
+            organization_id: organizationId,
             member_id: memberId,
-            mfa_phone_number: phone_number
+            mfa_phone_number: phoneNumber
         });
 
         res.status(200).json({ message: "OTP sent", redirectPath: `/smsmfa?sent=true&org_id=${resp.organization.organization_id}&member_id=${resp.member.member_id}` })
+
     } catch (error) {
         console.error("Could not send in callback", error);
-
         res.status(200).json({ message: "Could not send OTP", redirectPath: `/discovery` })
     }
 })
@@ -221,11 +229,11 @@ app.post("/api/mfa/authenticate", async (req, res) => {
         return
     }
 
-    const { orgID, memberId, code } = req.body;
+    const { organizationId, memberId, code } = req.body;
 
     try {
         const { session_jwt } = await stytchClient.otps.sms.authenticate({
-            organization_id: orgID,
+            organization_id: organizationId,
             member_id: memberId,
             code: code,
             intermediate_session_token: discoverySessionData.intermediateSession,
@@ -241,23 +249,23 @@ app.post("/api/mfa/authenticate", async (req, res) => {
 })
 
 app.post("/api/mfa/update", async (req, res) => {
-    const { organizationId, mfaPolicy } = req.body
+    const { organizationId, memberId, mfaOptIn } = req.body
 
     if (!organizationId || Array.isArray(organizationId)) {
-        res.status(500).json({ message: "No org ID found", redirectPath: "/login" });
-        return
-    }
-
-    if (!mfaPolicy || !(mfaPolicy === "REQUIRED_FOR_ALL" || mfaPolicy === "OPTIONAL")) {
-        res.status(500).json({ message: "Invalid value for MFA Policy", redirectPath: "/dashboard" });
+        res.status(500).json({ message: "No organization ID found", redirectPath: "/login" });
         return
     }
 
     try {
-        let resp = await stytchClient.organizations.update({ organization_id: organizationId, mfa_policy: mfaPolicy })
+        let resp = await stytchClient.organizations.members.update({ 
+            organization_id: organizationId, 
+            member_id: memberId, 
+            mfa_enrolled: mfaOptIn 
+        })
 
-        res.status(200).json({ message: "MFA policy updated successfully", organization: resp.organization })
+        res.status(200).json({ message: "MFA opt-in updated successfully", organization: resp.organization })
     } catch (e) {
+        console.error(r)
         res.status(500).json({ message: "Something went wrong" })
     }
 })
